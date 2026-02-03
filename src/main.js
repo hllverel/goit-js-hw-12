4f2a08eeeaffd41c139d56d47a886fb7f1dcdd73
hllverel <hllozdemir@live.com>
2026-02-04 00:04:17 +0300

    installed axios, cleaned code, added load more function

--- src/main.js
+++ src/main.js
@@ -4,22 +4,24 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
 
 const searchButton = document.querySelector('button[type="search"]');
+const loadMore = document.querySelector('button[class="loadmore"]');
 const form = document.querySelector("form");
 const loader = document.querySelector(".loader");
+const gallery = document.querySelector(".gallery");
 
-//const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchedItem) + "&image_type=photo&orientation=horizontal&safesearch=true";
+let currentPage = 1;
+let currentSearchItem = '';
 
-searchButton.addEventListener("click", (event) => {
+searchButton.addEventListener("click", async (event) => {
     event.preventDefault();
-
-    const gallery = document.querySelector(".gallery");
     gallery.innerHTML = '';
 
-    const searchedItem = document.querySelector("#image-search").value.trim();
+    currentSearchItem = document.querySelector("#image-search").value.trim();
 
-    if (searchedItem === '') {
+    if (currentSearchItem === '') {
         iziToast.error({
             position: 'topRight',
             message: 'Please enter your search query.',
@@ -27,39 +29,67 @@ searchButton.addEventListener("click", (event) => {
         return;
     };
 
+    await fetchPosts();
+    form.reset();
+});
+
+loadMore.addEventListener("click", async (event) => {
+    event.preventDefault();
+    currentPage += 1;
+    await fetchPosts();
+    });
+
+
+const fetchPosts = async () => {
     loader.style.display = "block";
 
     const params = new URLSearchParams({
         key: "54363861-75378e31e8cc289554c1794de",
-        q: searchedItem,
+        q: currentSearchItem,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: "true",
-        per_page: "99"
-    })
+        per_page: "18",
+        page: currentPage
+    });
 
-    fetch(`https://pixabay.com/api/?${params}`)
-        .then((response) => response.json())
-        .then((results) => {
-            loader.style.display = "none";
-            if (results.totalHits <= 0) {
-                iziToast.error({
-                    position: 'topRight',
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                });
-                return;
-            };
-            showResults(results.hits)
-        }) 
-        .catch((error) => {
-            loader.style.display = "none";
-            iziToast.error({
-                    position: 'topRight',
-                    message: `${error}`,
-                });
+    const totalPages = Math.ceil(100 / 18);
+
+    if (currentPage > totalPages) {
+        loader.style.display = "none";
+        iziToast.error({
+            position: 'topRight',
+            message: 'Sorry, end of results',
         });
+        loadMore.style.display = "none";
+        return;
+    };
+
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?${params}`);
+        const results = response.data;
+
+        loader.style.display = "none";
+        if (results.totalHits <= 0 || results.totalHits > 0 && results.totalHits <= 18) {
+            iziToast.error({
+                position: 'topRight',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+            });
+            loadMore.style.display = "none";
+            return;
+        };
+        showResults(results.hits);
+        loadMore.style.display = "block";
+    } catch (error) {
+        loader.style.display = "none";
+        iziToast.error({
+            position: 'topRight',
+            message: `${error}`,
+        });
+    }
+};
     
-    function showResults(results) {
+function showResults(results) {
         const searchResults = results
             .map((result) => {
                 return `
@@ -108,6 +138,3 @@ searchButton.addEventListener("click", (event) => {
 
         lightbox.refresh()
     }
-
-    form.reset()
-});
